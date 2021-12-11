0660cba5a6b428c07c1b99561ad14d5b9199f40c
Stepan Rabotkin <epicstyt@gmail.com>
2021-12-11 14:37:33 +0300

    feat: add coordinate distance

--- coordinates.go
+++ coordinates.go
@@ -1,6 +1,50 @@
 package geo
 
+import "math"
+
 // Coordinates is a Lat Long struct.
 type Coordinates struct {
-	Latitude, Longitude float64
+	Latitude, Longitude Radian
+}
+
+// NewCoordinatesFromDegrees return Coordinates from lat and long in Degree.
+func NewCoordinatesFromDegrees(latDeg, longDeg Degree) *Coordinates {
+	return &Coordinates{
+		Latitude:  latDeg.ToRadians(),
+		Longitude: longDeg.ToRadians(),
+	}
+}
+
+// NewCoordinatesFromRadians return Coordinates from lat and long in Radian.
+func NewCoordinatesFromRadians(latRads, longRads Radian) *Coordinates {
+	return &Coordinates{
+		Latitude:  latRads,
+		Longitude: longRads,
+	}
+}
+
+// EarthRadiusKilometers ...
+const EarthRadiusKilometers = 6371
+
+// Distance type.
+type Distance float64
+
+// ToKilometers convert Distance to Kilometers.
+func (d Distance) ToKilometers() float64 {
+	return float64(d) * EarthRadiusKilometers
+}
+
+// ToMeters convert Distance to Meters.
+func (d Distance) ToMeters() float64 {
+	return d.ToKilometers() * 1000
+}
+
+// Distance between two points in Coordinates.
+func (c *Coordinates) Distance(c2 *Coordinates) Distance {
+	sinLat := math.Sin((c2.Latitude.Float64() - c.Latitude.Float64()) / 2)
+	sinLng := math.Sin((c2.Longitude.Float64() - c.Longitude.Float64()) / 2)
+
+	a := sinLat*sinLat + math.Cos(c.Latitude.Float64())*math.Cos(c2.Latitude.Float64())*sinLng*sinLng
+
+	return Distance(2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a)))
 }
---
+++ coordinates_test.go
@@ -0,0 +1,96 @@
+package geo_test
+
+import (
+	"fmt"
+	"testing"
+
+	geo "github.com/EpicStep/go-simple-geo"
+)
+
+func TestCoordinates_Distance(t *testing.T) {
+	type test struct {
+		v    *geo.Coordinates
+		v2   *geo.Coordinates
+		want string
+	}
+
+	tests := []test{
+		{
+			v:    geo.NewCoordinatesFromDegrees(55.9606, 38.0456),
+			v2:   geo.NewCoordinatesFromDegrees(59.9386, 30.3141),
+			want: "634.70",
+		},
+		{
+			v:    geo.NewCoordinatesFromDegrees(55.7522, 37.6156),
+			v2:   geo.NewCoordinatesFromDegrees(54.7818, 32.0401),
+			want: "369.22",
+		},
+	}
+
+	for _, tt := range tests {
+		if x := fmt.Sprintf("%.2f", tt.v.Distance(tt.v2).ToKilometers()); x != tt.want {
+			t.Fatalf("incorrect answer for (%.2f %.2f) and (%.2f %.2f). Want: %s. Got: %s", tt.v.Latitude, tt.v.Longitude, tt.v2.Latitude, tt.v.Longitude, tt.want, x)
+		}
+	}
+}
+
+func TestDistance_ToKilometers(t *testing.T) {
+	type test struct {
+		v    geo.Distance
+		want string
+	}
+
+	tests := []test{
+		{
+			v:    geo.Distance(0.1),
+			want: "637.10",
+		},
+		{
+			v:    geo.Distance(1),
+			want: "6371.00",
+		},
+	}
+
+	for _, tt := range tests {
+		if x := fmt.Sprintf("%.2f", tt.v.ToKilometers()); x != tt.want {
+			t.Fatalf("incorrect answer for %.2f. Want: %s. Got: %s", float64(tt.v), tt.want, x)
+		}
+	}
+}
+
+func TestDistance_ToMeters(t *testing.T) {
+	type test struct {
+		v    geo.Distance
+		want string
+	}
+
+	tests := []test{
+		{
+			v:    geo.Distance(0.1),
+			want: "637100.00",
+		},
+		{
+			v:    geo.Distance(1),
+			want: "6371000.00",
+		},
+	}
+
+	for _, tt := range tests {
+		if x := fmt.Sprintf("%.2f", tt.v.ToMeters()); x != tt.want {
+			t.Fatalf("incorrect answer for %.2f. Want: %s. Got: %s", float64(tt.v), tt.want, x)
+		}
+	}
+}
+
+func BenchmarkCoordinates_Distance(b *testing.B) {
+	d := geo.NewCoordinatesFromDegrees(55.9606, 38.0456)
+	d2 := geo.NewCoordinatesFromDegrees(59.9386, 30.3141)
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		if dist := d.Distance(d2).ToKilometers(); dist != 634.6971900186131 {
+			b.Fail()
+		}
+	}
+}
